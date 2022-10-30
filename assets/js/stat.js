e0356564046b18a72bd5bacb9e0a902394bdbe28
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2022-10-30 02:26:34 +0000

    update stats

--- assets/js/stat.js
+++ assets/js/stat.js
@@ -1,4 +1,4 @@
-var txt = '{"release":["2021 Q3"],"price_uk":[1.04],"price_london":[-2.01],"afford_uk":[0.9],"afford_london":[-2.32]}';
+var txt = '{"release":["2022 Q3"],"price_uk":[1.51],"price_london":[-1.88],"afford_uk":[2],"afford_london":[-1.85]}';
 var obj = JSON.parse(txt);
 document.getElementById("js-release").innerHTML = "Release: <br>" + obj.release;
 document.getElementById("js-price-uk").innerHTML = obj.price_uk + " %";
